f9bb23254e15a8936e5978fe9fc694a1ce23b10f
code09-web <devendrasahu7000@gmail.com>
2021-11-09 17:22:13 +0530

    create notes models models

--- note/models.py
+++ note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from django .conf import settings
+from django.utils import timezone
+class NotesModel(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    body=models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    def __str__(self):
+         return self.title
